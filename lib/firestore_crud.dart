fc06cb98bfeb68e87564bdba1f9f80107aef7d62
Damodar Lohani <dlohani48@gmail.com>
2019-07-08 09:45:11 +0545

    firestore crud

---
+++ lib/firestore_crud.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'commons.dart' as commons;
+
+void firestoreCrud() {
+  addFirestorePlugin();
+  enableMultidex();
+  copyStockFiles();
+}
+
+void addFirestorePlugin() {
+  commons.addDependencise("  cloud_firestore: ${commons.loadConfig()['plugins']['firestore']}");
+  stdout.writeln("cloud firestore dependencies added"); 
+}
+
+void enableMultidex() {
+  String buildfile = commons.getFileAsString(commons.appBuildPath);
+  if(!buildfile.contains("multiDexEnabled")) {
+    buildfile = buildfile.replaceFirst(RegExp("defaultConfig.*{"), "defaultConfig {\n        multiDexEnabled true\n");
+    buildfile = buildfile.replaceFirst(RegExp("dependencies.*{"), "dependencies {\n    implementation 'androidx.multidex:multidex:2.0.1'");
+    commons.writeStringToFile(commons.appBuildPath, buildfile);
+  }
+}
+
+void copyStockFiles() {
+  commons.copyFilesRecursive('${commons.scriptRoot}/lib/firestore_crud_stock/lib', './', renameBaseDir: './lib');
+}
